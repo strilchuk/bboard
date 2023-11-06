e3ab5d2c3da65a8c132732589136d6f4051428e3
Alexander <strilchukalexander@gmail.com>
2023-11-06 15:36:25 +0300

    Наследование шаблонов

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Объявления</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-<body>
-<div class="container">
-    <h1 class="my-3 text-center">Объявления</h1>
+@extends('layouts.app')
+
+@section('title', 'Главная')
+
+@section('content')
     @if(count($bbs)>0)
         <table class="table table-striped table-berderless">
             <thead>
@@ -32,9 +25,4 @@
             </tbody>
         </table>
     @endif
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
